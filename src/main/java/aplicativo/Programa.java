fc82bccbc53a4f411122e7c649d1db0032bce9d0
Joao Angeloni <joaopedroangeloni@gmail.com>
2024-05-17 15:18:53 -0300

    centroDAO

--- src/main/java/aplicativo/Programa.java
+++ src/main/java/aplicativo/Programa.java
@@ -1,12 +1,13 @@
 package aplicativo;
 
-import dominio.CentroDeDistribuicao;
+import DAO.CentroDAO;
 
 public class Programa {
 	public static void main(String[] args) {
 		
-		CentroDeDistribuicao centros = new CentroDeDistribuicao();
-		centros.inserirCentros();
+	CentroDAO centroDAO = new CentroDAO();
+	centroDAO.inserirCentros();
 		
 	}
 }
+	
