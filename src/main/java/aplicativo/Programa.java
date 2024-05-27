34af434f8cb4a55dbfc2bcd4d29d308b9b4632b7
Joao Angeloni <joaopedroangeloni@gmail.com>
2024-05-27 12:52:51 -0300

    item 1

--- src/main/java/aplicativo/Programa.java
+++ src/main/java/aplicativo/Programa.java
@@ -4,16 +4,13 @@ package aplicativo;
 import Controller.EstoqueCentroController;
 import apresentacao.MenuPrincipal;
 import config.CentrosInstantiation;
+import config.DatabaseCleaner;
 import config.ItemInstantiation;
-import database.DatabaseCleaner;
 
 public class Programa {
 	public static void main(String[] args) {
 		DatabaseCleaner databaseCleaner = new DatabaseCleaner();
 		databaseCleaner.clearDatabase();
-		
-		//EstoqueCentroController ecc = new EstoqueCentroController();
-		//ecc.opcaoListar();
 		
 		CentrosInstantiation centroInstantiation = new CentrosInstantiation();
 		centroInstantiation.instanciar();
--- src/main/java/config/DatabaseCleaner.java
+++ src/main/java/config/DatabaseCleaner.java
@@ -1,4 +1,4 @@
-package database;
+package config;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
